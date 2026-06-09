81af148f898ed7cf7eff37d29a12c98fa626c98b
Venca Srubar <vaclavcarlosrubar@gmail.cz>
2026-06-09 10:45:15 +0200

    FInalized the basic CRUD

--- app/api/endpoints/items.py
+++ app/api/endpoints/items.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app.schemas.item import ItemResponse, ItemCreate
+from app.schemas.item import ItemResponse, ItemCreate, ItemUpdate
 from app.crud import item as crud_item
 from app.core.database import get_db 
 
@@ -15,3 +15,15 @@ def create_new_item(item_in: ItemCreate, db: Session = Depends(get_db)):
 @router.get("/", response_model=list[ItemResponse])
 def read_items(db: Session = Depends(get_db)):
     return crud_item.get_items(db=db)
+
+@router.get("/{id}", response_model=ItemResponse)
+def get_item(id: int, db: Session=Depends(get_db)):
+    return crud_item.get_item(id=id,db=db)
+
+@router.delete("/{id}", response_model=ItemResponse)
+def delete_item(id: int, db: Session=Depends(get_db)):
+    return crud_item.delete_item(id=id,db=db)
+
+@router.put("/{id}", response_model=ItemUpdate)
+def update_item(id: int, db: Session=Depends(get_db), item_data=ItemUpdate):
+    return crud_item.update_item(id=id, db=db, item_data=item_data)
--- app/crud/item.py
+++ app/crud/item.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from app.models.item import Item
-from app.schemas.item import ItemCreate
+from app.schemas.item import ItemCreate, ItemUpdate
 
 def create_item(db: Session, item_data: ItemCreate):
     db_item = Item(**item_data.model_dump())
@@ -11,3 +11,34 @@ def create_item(db: Session, item_data: ItemCreate):
 
 def get_items(db: Session, skip: int = 0, limit: int = 10):
     return db.query(Item).offset(skip).limit(limit).all()
+
+def get_item(id: int, db: Session):
+    item = db.query(Item).filter(Item.id == id).first()
+    return item
+
+def delete_item(id: int, db: Session):
+    item = db.query(Item).filter(Item.id == id).first()
+    db.delete(item)
+    db.commit()
+    db.refresh(item)
+    return item
+
+def update_item(id: int, db: Session, item_data: ItemUpdate):
+    db_item = db.query(Item).filter(Item.id == id).first()
+    
+    if not db_item:
+        return None  
+
+    update_data = item_data.model_dump(exclude_unset=True)
+
+    for key, value in update_data.items():
+        setattr(db_item, key, value)
+
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    
+    return db_item
+
+
+    
