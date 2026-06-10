0f8357d977db6d36eaf382f2ccc6230b05d8d18b
Venca Srubar <vaclavcarlosrubar@gmail.cz>
2026-06-10 11:05:18 +0200

    Add by-type statistics

--- app/api/endpoints/stats.py
+++ app/api/endpoints/stats.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy import func
 from sqlalchemy.orm import Session
 from app.core.database import get_db
-from app.models.item import Item
+from app.models.item import Item, ItemType
 
 router = APIRouter(
     prefix="/stats"
@@ -10,7 +10,6 @@ router = APIRouter(
 
 @router.get("/overview")
 def get_stats_overview(db: Session = Depends(get_db)):
-    # 1. Tvoje super napsané dotazy do DB
     items_seen = db.query(Item).filter(Item.completed == True).count()
     items_not_seen = db.query(Item).filter(Item.completed == False).count()
     avg_rating = db.query(func.avg(Item.rating)).filter(Item.completed == True).scalar()
@@ -24,4 +23,16 @@ def get_stats_overview(db: Session = Depends(get_db)):
         "total_completed": items_seen,
         "total_in_queue": items_not_seen,
         "average_rating": final_rating
+    }
+
+@router.get("/by-type")
+def get_stats_by_type(db: Session = Depends(get_db)):
+    books_count = db.query(Item).filter(Item.item_type == ItemType.BOOK).count()
+    movies_count = db.query(Item).filter(Item.item_type == ItemType.MOVIE).count()
+    albums_count = db.query(Item).filter(Item.item_type == ItemType.ALBUM).count()
+
+    return {
+        "books": books_count,
+        "movies": movies_count,
+        "albums": albums_count
     }
